132216e0bf344dff84c03c5e5b8add30ea796d13
LordMascachapas <alfredm3007@gmail.com>
2018-10-05 12:34:17 +0200

    Devuelve un json con fecha y hora

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res) {
+	console.log("Got a json for the homepage");
+	var dateString = Date();
+	res.send(JSON.stringify({ data: dateString }));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3001, function () {
+	console.log("Example app listening on port 3001!");
 });
